07d79f10a1c29dfdd6ec60bf27c6267dcd148e05
antonioarieiro <sfgfffs@gmail.com>
2022-02-11 21:26:15 -0300

    :sparkles: intial style

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Container  } from './styles';
+
+
+const Header: React.FC = () => {
+ 
+  return (
+      <Container>
+          
+      </Container>
+  )
+};
+
+export default Header;
