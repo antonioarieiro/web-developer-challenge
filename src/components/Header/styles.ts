0d5c598f690ae6c2b5b0947761799a1677ef0c49
antonioarieiro <sfgfffs@gmail.com>
2022-02-11 22:38:27 -0300

    :sparkles: finish style for header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div `
-  height: 60px;
-  background: #7159c1;
-  
-
+  height: 120px;
+  background: #2b2b2b;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+  > img {
+    width: 103px;
+    height: 45px;
+    object-fit: contain;
+  }
 `
