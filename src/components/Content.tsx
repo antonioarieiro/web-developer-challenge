8136ab1c90b2f69674bd7267d403a080ff92ce09
antonioarieiro <sfgfffs@gmail.com>
2022-02-11 21:10:47 -0300

    :sparkles: create content initial component

---
+++ src/components/Content.tsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+
+
+const Content: React.FC = () => {
+    const [publications, setPublications] = useState([]);
+    console.log(publications)
+    return(
+        <>
+        </>
+    )
+}
+
+export default Content;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react';
+
+
+const Header: React.FC = () => {
+    const [publications, setPublications] = useState([]);
+    console.log(publications)
+    return(
+        <>
+        </>
+    )
+}
+
+export default Header;
