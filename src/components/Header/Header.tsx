4c528033b8079dae897bb343f669298c3cfe42b6
antonioarieiro <sfgfffs@gmail.com>
2022-02-11 22:38:08 -0300

    create component header using logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Container  } from './styles';
-
+import Logo from '../../images/logo.png'
 
 const Header: React.FC = () => {
  
   return (
       <Container>
-          
+          <img src={Logo} alt="test"/>
       </Container>
   )
 };
